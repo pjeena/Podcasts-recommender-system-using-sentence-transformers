28c8b034b967bbefccb76f7e14822a57a47c79e1
Piyush Jeena <56345075+pjeena@users.noreply.github.com>
2023-01-19 09:35:49 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-PODCASTS_DATABASE_PATH_RAW='/Users/piyush/Desktop/dsml_Portfolio/podcast/final_push/data/raw/podcasts.csv'
-USER_REVIEWS_DATABASE_PATH_RAW='/Users/piyush/Desktop/dsml_Portfolio/podcast/final_push/data/raw/user_reviews.csv'
+PODCASTS_DATABASE_PATH_RAW='....../data/raw/podcasts.csv'
+USER_REVIEWS_DATABASE_PATH_RAW='....../data/raw/user_reviews.csv'
 
-PODCASTS_DATABASE_PATH_PROCESSED='/Users/piyush/Desktop/dsml_Portfolio/podcast/final_push/data/processed'
-USER_REVIEWS_DATABASE_PATH_PROCESSED='/Users/piyush/Desktop/dsml_Portfolio/podcast/final_push/data/processed'
+PODCASTS_DATABASE_PATH_PROCESSED='....../data/processed'
+USER_REVIEWS_DATABASE_PATH_PROCESSED='....../data/processed'
